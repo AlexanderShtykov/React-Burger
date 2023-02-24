5e399c98de88d90b595bdde9ad966dc3830677de
AlexanderShtykov <shtykovalexander@gmail.com>
2023-02-24 23:29:38 +0400

    init commit

---
+++ src/components/App.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Header from "./Header";
+import Order from "./Order";
+import MenuAdmin from "./MenuAdmin";
+import sampleBurgers from "../sample-burgers";
+
+class App extends React.Component {
+  state = {
+    burgers: {},
+    order: {},
+  };
+
+  addBurger = (burger) => {
+    const burgers = { ...this.state.burgers };
+    burgers[`burger${Date.now()}`] = burger;
+    this.setState({ burgers: burgers });
+  };
+
+  loadSampleBurgers = () => {
+    this.setState({ burgers: sampleBurgers });
+  };
+
+  render() {
+    return (
+      <div className="burger-paradise">
+        <div className="menu">
+          <Header title="Very Hot Burger" amount={10} hot={true} />
+        </div>
+        <Order />
+        <MenuAdmin
+          addBurger={this.addBurger}
+          loadSampleBurgers={this.loadSampleBurgers}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
