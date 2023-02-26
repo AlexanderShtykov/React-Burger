92e9feca9c0e1c6ce1421b9d54e7260cf15d3c92
AlexanderShtykov <shtykovalexander@gmail.com>
2023-02-26 19:45:32 +0400

    Integration with google firebase

---
+++ src/base.js
@@ -0,0 +1,14 @@
+import Rebase from "re-base";
+import firebase from "firebase";
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyBhx1KS-ebUgo9BrN7cxnWAXoM1Ul_fF1Y",
+  authDomain: "react-burger-fc3ea.firebaseapp.com",
+  databaseURL: "https://react-burger-fc3ea-default-rtdb.firebaseio.com",
+});
+
+const base = Rebase.createClass(firebaseApp.database());
+
+export { firebaseApp };
+
+export default base;
