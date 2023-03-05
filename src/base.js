e1ac6d86f9a6fbb21c6ec0651b16f7cdaedc25a3
AlexanderShtykov <shtykovalexander@gmail.com>
2023-03-05 17:19:11 +0400

    Adding PropTypes

--- src/base.js
+++ src/base.js
@@ -1,5 +1,6 @@
 import Rebase from "re-base";
-import firebase from "firebase";
+import firebase from "firebase/app";
+require("firebase/database");
 
 const firebaseApp = firebase.initializeApp({
   apiKey: "AIzaSyBhx1KS-ebUgo9BrN7cxnWAXoM1Ul_fF1Y",
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const Header = ({ title }) => (
   <header className="top">
@@ -21,5 +22,9 @@ const Header = ({ title }) => (
     </div>
   </header>
 );
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
 
 export default Header;
